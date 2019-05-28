35cba7b3359e49c88f29a900db543fb630b6bae5
prac1k <i.prac1k@gmail.com>
2019-05-28 13:21:04 +0300

    added icons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UsersList, UserEdit, UserCreate } from './components/colegues.js';
 import { PostList, PostEdit, PostCreate} from './components/boards.js';
+import PostIcon from '@material-ui/icons/Work';
+import UserIcon from '@material-ui/icons/Face';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
         <Admin dataProvider={dataProvider}>
-                <Resource name="users" list={UsersList} edit={UserEdit} create={UserCreate} />
-                <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+                <Resource name="users" list={UsersList} edit={UserEdit} create={UserCreate} icon={UserIcon}/>
+                <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
             </Admin>
     );
 
