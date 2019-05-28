7597d785a8511a989a74f7f4c1a022e6da69c0d1
prac1k <i.prac1k@gmail.com>
2019-05-28 13:59:44 +0300

    added dashboard and login page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { UsersList, UserEdit, UserCreate } from './components/colegues.js';
 import { PostList, PostEdit, PostCreate} from './components/boards.js';
 import PostIcon from '@material-ui/icons/Work';
 import UserIcon from '@material-ui/icons/Face';
+import Dashboard from './components/dashboard.js';
+import authProvider from './components/authProvider';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-        <Admin dataProvider={dataProvider}>
-                <Resource name="users" list={UsersList} edit={UserEdit} create={UserCreate} icon={UserIcon}/>
-                <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
-            </Admin>
-    );
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="users" list={UsersList} edit={UserEdit} create={UserCreate} icon={UserIcon}/>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    </Admin>
+);
 
 export default App;
