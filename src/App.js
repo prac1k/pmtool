2c5ea6864ad80665ec07398e0691701d9e2a08b4
prac1k <i.prac1k@gmail.com>
2019-05-28 10:51:22 +0300

    add boards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UsersList } from './components/colegues.js';
+import { BoardsList } from './components/boards'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
         <Admin dataProvider={dataProvider}>
                 <Resource name="Users" list={UsersList} />
-                <Resource name="Boards" list={ListGuesser} />
-                <Resource name="My Cards" list={ListGuesser} />
+                <Resource name="Boards" list={BoardsList} />
             </Admin>
     );
 
--- src/components/colegues.js
+++ src/components/colegues.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import CompanySiteField from './CompanySiteField';
+import { List, Datagrid, TextField, EmailField} from 'react-admin';
 
     export const UsersList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
+            <TextField source="name"/>
             <EmailField source="email" />
-            <TextField source="address.street" />
             <TextField source="phone" />
-            <TextField source="website" />
+            <CompanySiteField source="website" />
             <TextField source="company.name" />
         </Datagrid>
     </List>
