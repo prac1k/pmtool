e7ef76f094b72becb273f9de8545c1a7e4134407
prac1k <i.prac1k@gmail.com>
2019-07-05 11:00:45 +0300

    starting modal windows

--- catfish/client/src/components/Card.vue
+++ catfish/client/src/components/Card.vue
@@ -6,9 +6,22 @@
     @dragstart="onCardDragStart(index, $event)"
     @dragend="onCardDragEnd"
   >
-    <div class="card-title">{{ card.title }}</div>
-    <div class="card-body">{{ card.body }}</div>
-  </div>
+    <div>
+      <transition name="modal">
+        <div v-if="isOpen">
+          <div class="overlay" @click.self="isOpen = false;">
+            <div class="modal">
+              <div class="card-title">{{ card.title }}</div>
+              <div class="card-body">{{ card.body }}</div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+    <div class="card-title"@click="isOpen = !isOpen;">{{ card.title }} {{ isOpen ? "" : "" }}</div>
+    <div class="card-body" @click="isOpen = !isOpen;">{{ card.body }} {{ isOpen ? "" : "" }}</div>
+    </div>
+
 </template>
 
 <script>
@@ -21,6 +34,7 @@
     ],
     data() {
       return {
+        isOpen: false,
         card: null,
         list: null,
         isDraggingCard: false,
@@ -71,4 +85,51 @@
       z-index: 1;
     }
   }
+  .modal {
+    width: 500px;
+    margin: 0px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px 3px;
+    transition: all 0.2s ease-in;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  .fadeIn-enter {
+    opacity: 0;
+  }
+
+  .fadeIn-leave-active {
+    opacity: 0;
+    transition: all 0.2s step-end;
+  }
+
+  .fadeIn-enter .modal,
+  .fadeIn-leave-active.modal {
+    transform: scale(1.1);
+  }
+  button {
+    padding: 7px;
+    margin-top: 10px;
+    background-color: green;
+    color: white;
+    font-size: 1.1rem;
+  }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background: #00000094;
+    z-index: 999;
+    transition: opacity 0.2s ease;
+  }
+
+
+
 </style>
